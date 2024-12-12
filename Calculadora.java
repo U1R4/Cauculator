ea839895da88c212634850df67b6deb1fd8d5c42
Uirá Cardoso <88112570+U1R4@users.noreply.github.com>
2024-12-12 10:48:58 -0300

    EN_Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -9,31 +9,32 @@ public class Calculadora {
         int op = 0;
         double resp = 0;
         int loop = 1;
+	    
         while (loop == 1) {
             Scanner calc = new Scanner(System.in);
             System.out.println("__________________________________________________");
-            System.out.println("Qual Operacao vamos fazer hj?");
+            System.out.println("What operation are we going to do today?");
             System.out.println("__________________________________________________");
-            System.out.println("[ 1 ] Adicao");
-            System.out.println("[ 2 ] Subtracao");
-            System.out.println("[ 3 ] Divisao");
-            System.out.println("[ 4 ] Multiplicacao");
+            System.out.println("[ 1 ] Addition");
+            System.out.println("[ 2 ] Subtraction");
+            System.out.println("[ 3 ] Division");
+            System.out.println("[ 4 ] Multiplication");
             System.out.println("__________________________________________________");
 
-            System.out.println("Digite o numero correspondente da operacao escolhida:");
+            System.out.println("Enter the corresponding number of the chosen operation:");
 
             op = calc.nextInt();
             System.out.println("__________________________________________________");
 
             switch (op) {
                 case 1:
-                    System.out.println("Digite o primeiro numero:");
+                    System.out.println("Enter the first number:");
                     n1 = calc.nextDouble();
-                    System.out.println("Digite o segundo numero:");
+                    System.out.println("Enter the second number:");
                     n2 = calc.nextDouble();
                     resp = n1 + n2;
                     System.out.println("__________________________________________________");
-                    System.out.println("O resulltado da sua soma e: " + resp);
+                    System.out.println("The result of their sum is: " + resp);
                     break;
 
                 case 2:
@@ -43,39 +44,39 @@ public class Calculadora {
                     n2 = calc.nextDouble();
                     resp = n1 - n2;
                     System.out.println("__________________________________________________");
-                    System.out.println("O resulltado da sua subtracao e: " + resp);
+                    System.out.println("The result of your subtraction is: " + resp);
                     break;
 
                 case 3:
-                    System.out.println("Digite o primeiro numero:");
+                    System.out.println("Enter the first number:");
                     n1 = calc.nextDouble();
-                    System.out.println("Digite o segundo numero:");
+                    System.out.println("Enter the second number:");
                     n2 = calc.nextDouble();
                     resp = n1 / n2;
                     System.out.println("__________________________________________________");
-                    System.out.println("O resulltado da sua divisao e: " + resp);
+                    System.out.println("The result of your division is: " + resp);
                     break;
 
                 case 4
                         :
-                    System.out.println("Digite o primeiro numero:");
+                     System.out.println("Enter the first number:");
                     n1 = calc.nextDouble();
-                    System.out.println("Digite o segundo numero:");
+                    System.out.println("Enter the second number:");
                     n2 = calc.nextDouble();
                     resp = n1 * n2;
                     System.out.println("__________________________________________________");
-                    System.out.println("O resulltado da sua multiplicacao e: " + resp);
+                    System.out.println("The result of your multiplication is: " + resp);
                     break;
 
                 default:
-                    System.out.println("Opcao invalida...");
+                    System.out.println("Option invalid...");
 
             }
             System.out.println("__________________________________________________");
-            System.out.println("Mais uma conta para fazer? [ 1 ] Sim / [ 2 ] Nao");
+            System.out.println("Do you have one more calculation to do? [ 1 ] Yes / [ 2 ] No");
             loop = calc.nextInt();
             System.out.println("__________________________________________________");
-			System.out.println("Obrigado por usar nossa Calculadora\n");
+			System.out.println("Thank you for using our Calculator\n");
         }
     }
 }
